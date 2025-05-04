5e9949d5e0e672378e624322d0b7ee2bcf8dc61b
Leonardo Cserny <leo.cserny@gmail.com>
2025-05-04 21:19:44 +0300

    WIP

--- cmd/startpoc/main.go
+++ cmd/startpoc/main.go
@@ -4,35 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 	"github.com/lcserny/go-authservice/pkg/mongodb"
 	"github.com/lcserny/go-authservice/src/config"
 	"github.com/lcserny/go-authservice/src/logging"
 	"github.com/lcserny/go-authservice/src/web"
-	slogchi "github.com/samber/slog-chi"
 )
 
 func main() {
 	cfg := config.NewConfig()
-
-	repoProvider := mongodb.NewMongoRepositoryProvider(cfg)
-
-	r := web.InitRouter(cfg, repoProvider)
-	// FIXME: cant add Use() middleware after controllers are added in router, provide a custom NewRouter()?
-	r.Use(slogchi.New(logging.Logger()))
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.RequestID)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-	}))
-
+	db := mongodb.NewMongoRepositoryProvider(cfg)
+	router := web.NewRouter(cfg, db)
 	logging.Info(fmt.Sprintf("Starting %s on port: %d", cfg.Application.Name, cfg.Application.Port))
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.Application.Port), r)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.Application.Port), router)
 	if err != nil {
 		println(err.Error())
 	}
--- src/web/routes.go
+++ src/web/routes.go
@@ -1,7 +1,11 @@
 package web
 
 import (
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/lcserny/go-authservice/src/generated"
+	"github.com/lcserny/go-authservice/src/logging"
+	slogchi "github.com/samber/slog-chi"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,11 +14,31 @@ import (
 	"github.com/lcserny/go-authservice/src/users"
 )
 
-func InitRouter(cfg *config.Config, repoProvider db.RepositoryProvider) chi.Router {
+func NewRouter(cfg *config.Config, repoProvider db.RepositoryProvider) chi.Router {
 	var controllers []generated.Router
 	controllers = append(controllers, userControllers(cfg, repoProvider)...)
 	controllers = append(controllers, authControllers(cfg, repoProvider)...)
-	return generated.NewRouter(controllers...)
+
+	r := chi.NewRouter()
+	r.Use(slogchi.New(logging.Logger()))
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+	}))
+
+	for _, api := range controllers {
+		for _, route := range api.Routes() {
+			var handler http.Handler = route.HandlerFunc
+			r.Method(route.Method, route.Pattern, handler)
+		}
+	}
+
+	return r
 }
 
 func userControllers(cfg *config.Config, repoProvider db.RepositoryProvider) []generated.Router {
